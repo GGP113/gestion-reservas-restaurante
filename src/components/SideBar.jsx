f1b19c127c40b33ae5934323badd61af5677a58e
GGP113 <paulaag316@gmail.com>
2026-05-25 22:41:44 -0500

    feature: se agrega la pagina de reservas y el sideBar ára navegación

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "../styles/SideBarStyle.css";
+
+import { getLocalStorage, removeLocalStorage } from "../helpers/local-storage";
+import { redirectAlert } from "../helpers/alerts";
+
+import { Link } from "react-router-dom";
+
+function SideBar() {
+  
+
+  return (
+    <aside className="sidebar-container">
+      {/* Bloque Superior */}
+      <div>
+        <div className="sidebar-title">
+          Anfitrión:
+          <span className="sidebar-host-name">[Nombre Aquí]</span>
+        </div>
+
+        {/* Botón de navegación (reutiliza el estilo de tu CSS) */}
+        <div id="endform">
+          <button type="button">Ver Reservas</button>
+        </div>
+      </div>
+
+      {/* Bloque Inferior: Botón de Cerrar Sesión */}
+      <div id="endform">
+        <button type="button">Cerrar Sesión</button>
+      </div>
+    </aside>
+  );
+}
+
+export default SideBar;
--- src/pages/PAnelReservas.jsx
+++ src/pages/PAnelReservas.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import "../styles/LogInStyle.css";
+import SideBar from "../components/SideBar";
 
 function PAnelReservas() {
   return (
-    <div>PAnelReservas</div>
-  )
+    <div className="reservas-page-container">
+      {/* 1. Barra lateral fija */}
+      <SideBar />
+
+      {/* 2. Contenido principal */}
+      <main className="reservas-main-content">
+        <form className="form">
+          <div id="circleform"></div>
+          <div id="introform">
+            <div id="introformbehind">
+              <p>||||||||||||||||||||||||||||||||||||</p>
+            </div>
+          </div>
+
+          <div id="middleform">
+            <p>Fecha</p>
+            <input className="input" type="text" required placeholder="" />
+
+            <p>Turno</p>
+            <input />
+
+            <p>Cantidad de personas</p>
+            <input />
+          </div>
+
+          <div id="endform">
+            <button type="submit">Crear</button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
 }
 
-export default PAnelReservas
+export default PAnelReservas;
